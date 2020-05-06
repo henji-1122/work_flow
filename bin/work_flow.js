e9e2e518b2603782a71b93bdff8944f311c05433
Wangxc <2475886163@qqq.com>
2020-05-06 10:27:44 +0800

    feat:updata package

--- bin/work_flow.js
+++ bin/work_flow.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node 
 
 process.argv.push('--cwd')
-process.argv.push(process.cwd())
-process.argv.push('--gulpfile')
-process.argv.push(require.resolve('..'))
+process.argv.push(process.cwd())  // 工作目录
+process.argv.push('--gulpfile')  //gulpfile所在路径
+process.argv.push(require.resolve('..'))  //work_flow.js路径:work_flow/lib/work_flow.js 
 
 console.log(process.argv)
 require('gulp/bin/gulp')
